403e07c6fa02cf4f500e76cba12609b61d91749a
raffaelecapaldo <raffaelecpl@gmail.com>
2023-03-10 16:19:53 +0100

    Bonus completed.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 /* Scrivi un programma che stampi in console i numeri da 1 a 100,
 ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
-
+//VARIABILE CONTENTE IL DIV BOX-CONTAINER
 const boxContainer = document.getElementById("box-container");
 
 
 //CICLO FOR PER ITERARE 100 VOLTE
 for (let i = 1; i < 101; i++) {
-
-    const fizzHtml = `<div class="box"><span>Fizz</span></div>`;
-    const buzzHtml = `<div class="box"><span>Buzz</span></div>`;
-    const fizzBuzzHtml = `<div class="box"><span>FizzBuzz</span></div>`;
-    const numberHtml = `<div class="box"><span>${i}</span></div>`;
+    //VARIABILI CONTENENTE HTML DA INSERIRE PER OGNI TIPO DI BOX
+    const fizzHtml = `<div class="box bg-fizz"><span>Fizz</span></div>`;
+    const buzzHtml = `<div class="box bg-buzz"><span>Buzz</span></div>`;
+    const fizzBuzzHtml = `<div class="box bg-fizzbuzz"><span>FizzBuzz</span></div>`;
+    const numberHtml = `<div class="box bg-number"><span>${i}</span></div>`;
     //SE SONO MULTIPLI DI 5 o 3 STAMPARE FIZZBUZZ
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("FizzBuzz")
@@ -20,7 +20,7 @@ for (let i = 1; i < 101; i++) {
     //SE SONO MULTIPLI DI 3 STAMPARE FIZZ
     else if (i % 3 == 0) {
         console.log("Fizz")
-       boxContainer.innerHTML += fizzHtml;
+        boxContainer.innerHTML += fizzHtml;
 
     }
     //SE SONO MULTIPLI DI 5 STAMPARE BUZZ
